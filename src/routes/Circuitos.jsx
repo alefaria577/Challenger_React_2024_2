5bd919a8d7984b50c87a8d149f955219836a64b1
Alexandre <Alexandresilvadefaria577@gmail.com>
2024-09-30 12:32:08 -0300

    adicionando circuitos

--- src/routes/Circuitos.jsx
+++ src/routes/Circuitos.jsx
@@ -1,10 +1,89 @@
+import '../css/Circuitos.css'
+
+import berlin from '../imgs_circuito/berlin-bandeira.png'
+import berlin_corrida from '../imgs_circuito/berlin-corrida.png'
+import londres from '../imgs_circuito/londres-bandeira.png'
+import londres_corrida from '../imgs_circuito/londres-corrida.png'
+import monaco from '../imgs_circuito/monaco-bandeira.png'
+import monaco_corrida from '../imgs_circuito/monaco-corrida.png'
+import portland from '../imgs_circuito/portland-corrida.png'
+import portland_corrida from '../imgs_circuito/portland-corrida.png'
+import shangai from '../imgs_circuito/shangai-bandeira.png'
+import shangai_corrida from '../imgs_circuito/shangai-corrida.png'
+
 
 const Circuitos =()=>{
 
     return(
         <>
 
-            <h1>Circuitos</h1>
+<div className="titulo_circuitos"> Circuitos</div>
+
+<div className="box_c">
+    
+    <div className="box_circuito">
+        <div className="titulo_box_circuito">Próxima corrida</div>
+
+        <div className="traco_circuitos"></div>
+        <div className="imagem_circuitos"><img src={portland} alt=""/></div>
+        
+    </div>
+
+    <div className="box_circuito">
+        <div className="titulo_box_circuito">
+
+            <div className="bandeiras_circuito"><img src={berlin} alt=""/></div>
+            
+            Berlim</div>
+            <div className="traco_circuitos"></div>
+            <div className="imagem_circuitos"><img src={berlin_corrida} alt=""/></div>
+    </div>
+
+    <div className="box_circuito">
+        <div className="titulo_box_circuito">
+            
+            <div className="bandeiras_circuito"><img src={londres} alt=""/></div>
+            
+        Londres</div>
+        <div className="traco_circuitos"></div>
+        <div className="imagem_circuitos"><img src={londres_corrida} alt=""/></div>
+    </div>
+</div>
+
+<div className="box_c">
+    
+    <div className="box_circuito">
+        <div className="titulo_box_circuito">
+        
+            <div className="bandeiras_circuito"><img src={monaco} alt=""/></div>
+            Monaco</div>
+
+            <div className="traco_circuitos"></div>
+        
+            <div className="imagem_circuitos"><img src={monaco_corrida} alt=""/></div>
+
+    </div>
+
+    <div className="box_circuito">
+        <div className="titulo_box_circuito">
+
+            <div className="bandeiras_circuito"><img src={portland} alt=""/></div>
+            
+        Portland</div>
+        <div className="traco_circuitos"></div>
+        <div className="imagem_circuitos"><img src={portland_corrida} alt=""/></div>
+    </div>
+
+    <div className="box_circuito">
+        <div className="titulo_box_circuito">
+            
+            <div className="bandeiras_circuito"><img src={shangai} alt=""/></div>
+            
+        Shangai</div>
+        <div className="traco_circuitos"></div>
+        <div className="imagem_circuitos"><img src={shangai_corrida} alt=""/></div>
+    </div>
+</div>
 
         
         
