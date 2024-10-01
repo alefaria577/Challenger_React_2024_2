a476b4070ccb40742a78cb967a651e08c67a148d
Alexandre <Alexandresilvadefaria577@gmail.com>
2024-09-30 21:01:08 -0300

    adicionando responsividade home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,30 +2,25 @@ import '../css/Home.css'
 import carro_inicio from '../imagens/carro-inicio.png'
 
 const Home = () => {
-
-return (
-
-    <>
-    
-        <main>       
-        <div className="barra1"></div>
-        <div className="barra2"></div> 
-            <div className="corpo_home">                
-                <p>FormulaE a corrida com carros</p>
-                <p>elétricos de alta</p>
-                <p>performance</p>
-                <p className="saber_mais">Saber Mais</p>               
-            </div>
-            <aside>
-                <img src={carro_inicio} alt="imagem do carro"/>
-            </aside>
-        </main>
-    
-    </>
-
-
-
-);
+    return (
+        <>
+            <main>
+                <div className="barra1"></div>
+                <div className="barra2"></div>
+                <div className="grid_container"> {/*container de grid */}
+                    <div className="corpo_home">                
+                        <p>FormulaE a corrida com carros</p>
+                        <p>elétricos de alta</p>
+                        <p>performance</p>
+                        <p className="saber_mais">Saber Mais</p>               
+                    </div>
+                    <aside>
+                        <img src={carro_inicio} alt="imagem do carro" />
+                    </aside>
+                </div>
+            </main>
+        </>
+    );
 };
 
 export default Home;
