63a81dcab1155291fbfed68f8ea2e744ac41dc62
Alexandre <Alexandresilvadefaria577@gmail.com>
2024-10-07 17:16:04 -0300

    att para pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'; 
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Challenger_web/', // Defina o nome do seu repositório
-})
+  base: '/', // Para uso com GitHub Pages diretamente da branch main
+});
