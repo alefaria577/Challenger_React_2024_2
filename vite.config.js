d07fe60da083b493ecdb6a82597fc3d4de96a0ae
Alexandre <Alexandresilvadefaria577@gmail.com>
2024-10-07 16:12:31 -0300

    att vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Challenger_web/', // Defina o nome do seu repositório
 })
